6cbf0499867b30387b165f5001d2628e6cf55351
Harshal <hjbhangale2004@gmail.com>
2026-06-21 00:48:55 +0530

    Auto create admin user for Render deployment

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,5 +1,16 @@
 from django.apps import AppConfig
-
+from django.contrib.auth import get_user_model
 
 class AccountsConfig(AppConfig):
     name = 'accounts'
+
+    def ready(self):
+        User = get_user_model()
+
+        # create admin only if not exists
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser(
+                username="Harshal",
+                email="admin@test.com",
+                password="Hitesh@2013"
+            )
