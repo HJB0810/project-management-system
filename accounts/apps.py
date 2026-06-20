f77e9b25e3f058eebc6353da240d50bf3c144053
Harshal <hjbhangale2004@gmail.com>
2026-06-21 00:58:05 +0530

    Remove auto admin creation for security

--- accounts/apps.py
+++ accounts/apps.py
@@ -4,13 +4,3 @@ from django.contrib.auth import get_user_model
 class AccountsConfig(AppConfig):
     name = 'accounts'
 
-    def ready(self):
-        User = get_user_model()
-
-        # create admin only if not exists
-        if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser(
-                username="admin",
-                email="admin@test.com",
-                password="Hitesh@2013"
-            )
